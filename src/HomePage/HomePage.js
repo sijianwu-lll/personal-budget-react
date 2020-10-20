c2582b17133911d11fd66e28276e98efddaaf460
sijianwu <sijianwu3@gmail.com>
2020-10-19 23:14:46 -0400

    backend chart

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,4 +1,43 @@
 import React from 'react';
+import Chart from 'chart.js';
+import axios from 'axios';
+var dataSource = {
+    datasets: [
+        {
+            data: [],
+            backgroundColor: [
+                '#ffcd56',
+                '#ff6384',
+                '#36a2eb',
+                '#fd6b19',
+            ]
+        }
+    ],
+    labels: []
+};
+
+function createChart() {
+var ctx = document.getElementById('myChart').getContext('2d');
+var myPieChart = new Chart(ctx, {
+    type: 'pie',
+    data: dataSource
+});
+}
+
+function getBudget() {
+axios.get('http://localhost:5000/Budget')
+.then(function (res) {
+    for (var i = 0; i < res.data.myBudget.length; i++) {
+        dataSource.datasets[0].data[i] = res.data.myBudget[i].budget;
+        dataSource.labels[i] = res.data.myBudget[i].title;
+        createChart();
+    }
+});
+}
+
+getBudget();
+
+
 
 
 function HomePage() {
@@ -73,7 +112,11 @@ function HomePage() {
 
         </div>
 
+      
+
     </main>
+     
+    
   );
 }
 
